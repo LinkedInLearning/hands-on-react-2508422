5c01cd3cc7f5cbc7551f1545ff3eb7b2d648b5f8
Ray Villalobos <ray@planetoftheweb.com>
2022-08-28 11:54:21 +0000

    working on new structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  const name = 'Raybo'
+  return (
+    <div className="container">
+      <article>
+        <hgroup>
+          <h1>Welcome <i style={{ color: "SteelBlue" }}>{name}</i></h1>
+          <p>Lorem ipsum dolor sit<br />amet consectetur adipisicing elit. Aut nam dolorem ullam iusto nobis cupiditate corrupti, laborum eius fuga suscipit perferendis explicabo eveniet at officiis itaque quasi voluptatem! Laborum, rem.</p>
+        </hgroup> 
+      </article>
+    </div>
+  )
+}
+export default App
