f90151a9951a84761e45af1f20b1181b7bc63295
Ray Villalobos <ray@planetoftheweb.com>
2022-08-29 20:20:01 +0000

    End of Video Target

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 function App() {
-  const name = 'Raybo'
+  const name = 'StarGazers';
   return (
     <div className="container">
       <article>
+        <img src="images/group.svg" alt="StarGazer Group" />
         <hgroup>
-          <h1>Welcome <i style={{ color: "SteelBlue" }}>{name}</i></h1>
-          <p>Lorem ipsum dolor sit<br />amet consectetur adipisicing elit. Aut nam dolorem ullam iusto nobis cupiditate corrupti, laborum eius fuga suscipit perferendis explicabo eveniet at officiis itaque quasi voluptatem! Laborum, rem.</p>
-        </hgroup> 
+          <h1>Meet the <i style={{ color: "SteelBlue" }}>{name}</i></h1>
+          <p>Members of an <b>intergalactic alliance</b><br />
+            paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <button className="outline" onClick={() => console.log('Hi There')}>Click me</button>
+        </hgroup>
       </article>
     </div>
   )
