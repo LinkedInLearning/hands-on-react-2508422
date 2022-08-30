4959ddb810eaa01509b34c3d551cd7eb6bc07843
Ray Villalobos <ray@planetoftheweb.com>
2022-08-30 01:18:16 +0000

    01_01b

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 function App() {
-  const name = 'StarGazers';
   return (
-    <div className="container">
-      <article>
-        <img src="images/group.svg" alt="StarGazer Group" />
-        <hgroup>
-          <h1>Meet the <i style={{ color: "SteelBlue" }}>{name}</i></h1>
-          <p>Members of an <b>intergalactic alliance</b><br />
-            paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <button className="outline" onClick={() => alert('Hi There')}>Click me</button>
-        </hgroup>
-      </article>
-    </div>
+    <hgroup>
+      <h1>Meet the StarGazers</h1>
+      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+    </hgroup>
   )
 }
 export default App
