00ace12849c7623898eaeb35708961452b18fb56
Ray Villalobos <ray@planetoftheweb.com>
2022-08-30 14:47:08 +0000

    finished components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import React, { Component } from "react";
+
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: 'Star Geezers' };
+  }
+  render() {
+    return <h1>Meet the <i>{this.props.name}</i></h1>
+  }
+}
+
+const Hello = (props) => {
+  const localName = 'Star Team'
+  return (
+    <h1>Meet the <i>{props.name}</i></h1>
+  )
+}
+
 function App() {
-  const name = 'StarGazers';
   return (
     <div className="container">
       <article>
         <img src="images/group.svg" alt="StarGazer Group" />
         <hgroup>
-          <h1>Meet the <i style={{ color: "SteelBlue" }}>{name}</i></h1>
+          <Welcome name="Star Pozers" />
+          <Hello name="StarGazers" />
+
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
           <button className="outline" onClick={() => alert('Hi There')}>Click me</button>
