5a5501dc11c18df6ca73a53c531dfc8ed68de438
Ray Villalobos <ray@planetoftheweb.com>
2022-08-31 23:42:11 +0000

    01_04e

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+const pageTitle = document.title;
 
 export default () => {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    count && (document.title = `${pageTitle} - ${count}`)
+  })
   return (
     <button className="outline" onClick={() => setCount(count + 1)}>
-      {count === 0 ? "Click to support": `Supported ${count} times`}
+      {count === 0 ? "Click to support" : `Supported ${count} times`}
     </button>
   )
 }
