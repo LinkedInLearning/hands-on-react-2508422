12e1ba29e83ed7be18e1a8f2c48ae1b482195290
Ray Villalobos <ray@planetoftheweb.com>
2022-09-01 00:12:08 +0000

    01-05e

---
+++ src/components/ListCast.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+export default () => {
+  const [cast, setCast] = useState([]);
+
+  async function fetchCast() {
+    const response = await fetch('cast.json');
+    setCast(await response.json());
+  }
+
+  useEffect(() => {
+    fetchCast();
+  });
+
+  return (
+    <div style={{
+      display: "grid",
+      gridTemplateColumns: `repeat(auto-fit, minmax(90px, 1fr))`,
+      gap: `1rem`,
+      marginBottom: '1rem'
+    }}>
+      {
+        cast.map(member => (
+          <a key={member.id} data-tooltip={member.name}>
+            <img src={`images/${member.slug}_tn.svg`} alt={member.name} />
+          </a>
+        ))
+      }
+    </div>
+  )
+}
