82119fc24305340cd42143c0cf04e744b21ddde8
Ray Villalobos <ray@planetoftheweb.com>
2022-09-05 02:26:43 +0000

    02_02e

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component, useState } from 'react';
 import ListCast from './components/ListCast';
+import Modals from './components/Modals';
 
 function App() {
   const name = 'StarGazers'
@@ -11,22 +12,7 @@ function App() {
         <h1>Meet the Stargazers</h1>
         <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
         <ListCast onChoice={(info) => { setMemberInfo(info) }} />
-        {memberInfo &&
-          <article>
-            <hgroup>
-              <div style={{
-                display: "flex",
-                gap: '1rem'
-              }}>
-                <img style={{ width: '200px' }} src={`images/${memberInfo.slug}.svg`} alt={memberInfo.name} />
-                <hgroup>
-                  <h1>{memberInfo.name}</h1>
-                  <p>{memberInfo.bio}</p>
-                </hgroup>
-              </div>
-            </hgroup>
-          </article>
-        }
+        {memberInfo && <Modals member={memberInfo} handleClose={() => { setMemberInfo(null) }} />}
       </hgroup>
     </div>
   )
---
+++ src/components/Modals.jsx
@@ -0,0 +1,28 @@
+export default ({ member, handleClose }) => {
+  return (
+    <dialog id="modal-member" open>
+      <article>
+        <a href="#close"
+          aria-label="Close"
+          className="close"
+          data-target="modal-member"
+          onClick={handleClose}
+        ></a>
+        <hgroup>
+          <div style={{
+            display: "flex",
+            gap: '1rem'
+          }}>
+            <img style={{ width: '200px' }}
+              src={`images/${member.slug}.svg`}
+              alt={member.name} />
+            <hgroup>
+              <h1>{member.name}</h1>
+              <p>{member.bio}</p>
+            </hgroup>
+          </div>
+        </hgroup>
+      </article>
+    </dialog>
+  )
+}
