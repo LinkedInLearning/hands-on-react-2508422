cf6e101a22e2aac3cda7e69c162b1a81fe630c9c
Ray Villalobos <ray@planetoftheweb.com>
2022-09-05 21:22:31 +0000

    02_04e

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import ListCast from './components/ListCast';
 import Modals from './components/Modals';
+import Nav from './components/Nav';
 
 function App() {
-  const name = 'StarGazers'
   const [cast, setCast] = useState([]);
   let [memberInfo, setMemberInfo] = useState(null);
 
@@ -17,15 +17,18 @@ function App() {
   });
 
   return (
-    <div className="container">
-      <hgroup>
-        <img src="images/group.svg" alt="StarGazers Group" />
-        <h1>Meet the Stargazers</h1>
-        <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-        <ListCast cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
-        {memberInfo && <Modals member={memberInfo} handleClose={() => { setMemberInfo(null) }} />}
-      </hgroup>
-    </div>
+    <>
+      <Nav cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
+      <div className="container">
+        <hgroup>
+          <img src="images/group.svg" alt="StarGazers Group" />
+          <h1>Meet the Stargazers</h1>
+          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <ListCast cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
+          {memberInfo && <Modals member={memberInfo} handleClose={() => { setMemberInfo(null) }} />}
+        </hgroup>
+      </div>
+    </>
   )
 }
 export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,24 @@
+export default ({ cast, onChoice }) => {
+  return (
+    <nav className="container">
+      <ul>
+        <li>
+          <details role="list">
+            <summary aria-haspopup="listbox" role="link">cast</summary>
+            <ul role="listbox">
+              {cast.map(member => (
+                <li key={member.id}>
+                  <a onClick={() => { onChoice(member) }}
+                    data-tooltip={member.name}>{member.name}</a>
+                </li>
+              ))}
+            </ul>
+          </details>
+        </li>
+      </ul>
+      <ul>
+        <li><a href="#"><img style={{ height: '50px' }} src="images/logo_bug_stargazers.svg" alt="Stargazers Logo" /></a></li>
+      </ul>
+    </nav>
+  )
+}
