ba5615b9fbf3a82980bd592f683be5f5f0e4c85f
Ray Villalobos <ray@planetoftheweb.com>
2022-09-07 06:22:33 +0000

    03_04e

--- src/components/Modals.jsx
+++ src/components/Modals.jsx
@@ -19,13 +19,18 @@ export default ({ member, handleClose, handleChange }) => {
             <hgroup>
               <h1>{member.name}</h1>
               <p>{member.bio}</p>
-              <hgroup>
+              <hgroup style={{
+                display: "flex",
+                gap: "1rem",
+                marginTop: "1rem",
+                justifyContent: "space-between"
+              }}>
                 <a className="outline" href="#" role="button"
                   onClick={() => { handleChange(Number(member.id) - 1) }}
-                >previous</a>
+                ><span className="material-symbols-outlined">arrow_back</span></a>
                 <a className="outline" href="#" role="button"
                   onClick={() => { handleChange(Number(member.id) + 1) }}
-                >next</a>
+                ><span className="material-symbols-outlined">arrow_forward</span></a>
               </hgroup>
             </hgroup>
           </div>
