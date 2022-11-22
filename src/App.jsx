1c717e87fc9ff88a6c13bfaacf763df59348fa39
JasmandeepKaur <72990928+JasmandeepKaur@users.noreply.github.com>
2022-11-22 18:06:11 +0000

    Adding in Hello and Welcome components

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,8 @@ function App() {
       <article>
       <hgroup>
         <img src="images/group.svg" alt="StarGazers Group" />
-          <h1>Meet the <i style={{color:"SteelBlue"}}>{name}</i></h1>
+          <Welcome name="Stars" />
+          <Hello name="StarGazers" />
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. 
             They are known for their enthusiasm for science, for their love of fun, 
