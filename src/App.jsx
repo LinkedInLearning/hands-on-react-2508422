d55d6cfcdb4f533fff2d34b3bf7502a133cf3bd5
JasmandeepKaur <72990928+JasmandeepKaur@users.noreply.github.com>
2022-11-22 17:52:32 +0000

    Video activity

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 function App() {
+  const name = 'StarGazers'
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+    <div className="container">
+      <article>
+      <hgroup>
+        <img src="images/group.svg" alt="StarGazers Group" />
+          <h1>Meet the <i style={{color:"SteelBlue"}}>{name}</i></h1>
+          <p>Members of an <b>intergalactic alliance</b><br />
+            paving the way for peace and benevolence among all species. 
+            They are known for their enthusiasm for science, for their love of fun, 
+            and their dedication to education.</p>
+          <button className="outline" onClick={()=> alert('Hi there')}>Click Me</button>
+      </hgroup>
+      </article>
+    </div>
   )
 }
 export default App
