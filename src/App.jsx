a66cb371116f9c4f3ecce0c41076a5d33659c052
John Cañero <99738754+johncanero@users.noreply.github.com>
2022-11-23 02:51:47 +0000

    Component and Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,47 @@
+import React, { Component } from "react";
+
+// Difficult
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h1>Meet the {this.props.name}</h1>;
+  }
+}
+
+// Easier
+const Hello = (props) => {
+  return (
+    <h1>
+      Meet the <i>{props.name}</i>
+    </h1>
+  );
+};
+
 function App() {
-  const name = 'StarGazers'
+  const name = "StarGazers";
   return (
     <div className="container">
       <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <h1>Meet the <i style={{ color: "SteelBlue" }}>{name}</i></h1>
-          <p>Members of an <b>intergalactic alliance</b><br />
-            paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <button className="outline" onClick={() => alert('Hi there')}>Click Me</button>
+          <Welcome name="Stars" />
+          <Hello name="The Aliens" />
+          <Hello name="Stargazers" />
+          <p>
+            Members of an <b>intergalactic alliance</b>
+            <br />
+            paving the way for peace and benevolence among all species. They are
+            known for their enthusiasm for science, for their love of fun, and
+            their dedication to education.
+          </p>
+          <button className="outline" onClick={() => alert("Hi there")}>
+            Click Me
+          </button>
         </hgroup>
       </article>
     </div>
-  )
+  );
 }
-export default App
+export default App;
