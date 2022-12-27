9b2ca36799337bbb421bfb609358892309230457
Jessica Mansell <jcmansell88@gmail.com>
2022-12-27 23:13:50 +0000

    started second section

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import React, { Component } from 'react';
-import Welcome from './components/Welcome';
-import Support from './components/Support';
+import React, { Component, useState } from 'react';
 import ListCast from './components/ListCast';
 
 function App() {
   const name = 'StarGazers'
+  let [memberInfo, setMemberInfo] = useState(null);
   return (
     <div className="container">
       <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <Welcome name="StarGazers" />
-          <p>Members of an <b>intergalactic alliance</b><br />
-            paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <h1>Meet the StarGazers</h1>
+          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
           <ListCast />
-          <Support />
         </hgroup>
       </article>
     </div>
