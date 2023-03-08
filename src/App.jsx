948e74f68b53a4dc164ece25c693e88450e07b7e
ClaesF0 <claes_folkestad@hotmail.com>
2023-03-08 01:40:18 +0100

    Completed course, possible section for portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
+import React, {useEffect, useState} from "react";
+import ListCast from "./components/ListCast";
+import Modals from "./components/Modals";
+import Nav from './components/Nav'
+import "./App.scss"
+
 function App() {
-  const name = 'StarGazers'
+  const [cast, setCast] = useState([]);
+  let [memberInfo, setMemberInfo] = useState(null);
+
+  async function  fetchCast() {
+    const response = await fetch ('cast.json');
+    setCast(await response.json());
+  }
+  useEffect(()=> {
+    fetchCast();
+  });
+
   return (
-    <div className="container">
-      <article>
+    <>
+    <Nav cast={cast} onChoice={(info)=> {setMemberInfo(info)}}/>
+        <div className="container">
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <h1>Meet the <i style={{ color: "SteelBlue" }}>{name}</i></h1>
+          <h1>Meet the StarGazers</h1>          
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <button className="outline" onClick={() => alert('Hi there')}>Click Me</button>
+       <ListCast cast={cast} onChoice={(info)=> {setMemberInfo(info)}}/>
+       { memberInfo && <Modals member={memberInfo} handleChange={(info)=> {setMemberInfo(cast[info])}} handleClose={()=>{setMemberInfo(null)}}/>}
         </hgroup>
-      </article>
     </div>
+    </>
+
   )
 }
 export default App
---
+++ src/components/Modals.jsx
@@ -0,0 +1,46 @@
+export default ({member, handleClose, handleChange})=> {
+  return (
+    <dialog id="modal-member" open>
+      <article>
+        <a id="close"
+        aria-label="Close"
+        href=""
+        className="close"
+        data-target="modal-member"
+        onClick={handleClose}
+        ></a>
+        <hgroup>
+           <div style={{
+            display: 'flex', 
+            gap: '1rem'}}>
+              <img style={{
+                width: '200px'}} 
+                src={`images/${member.slug}_tn.svg`} 
+                alt={member.name}/>
+              <hgroup>
+              <h1>{member.name}</h1>
+              <p>{member.bio}</p>
+              <hgroup style={{
+                display:"flex",
+                gap: "1rem",
+                marginTop:"1rem",
+                justifyContent:"space-between"
+                }}>
+                <a className="outline" href="#" role="button"
+                onClick={()=> {handleChange(Number(member.id)-1)}}
+                ><span className="material-symbols-outlined">arrow_back</span>
+                </a>
+                <a className="outline" href="#" role="button"
+                onClick={()=> {handleChange(Number(member.id)+1)}}
+                ><span className="material-symbols-outlined">arrow_forward</span></a>
+              </hgroup>
+              </hgroup>             
+            </div>
+            <div>
+
+            </div>
+        </hgroup>
+      </article>
+    </dialog>
+  )
+} 
