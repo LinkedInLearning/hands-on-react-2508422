c00d5e4d57c750bc11f2aefc7b3bf7ada1043217
MaksigBor007 <amazonmaksig@gmail.com>
2023-04-06 13:50:28 -0600

    FIrst Lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import ListCast from './components/ListCast';
-import Modals from './components/Modals';
-import Nav from './components/Nav';
-import "./App.scss";
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h1>Meet the { this.props.name }</h1>;
+   }
+}
+
+const Hello = (props) => {
+  return <h1>Meet the <i>{props.name}</i></h1>
+}
 
 function App() {
-  const [cast, setCast] = useState([]);
-  let [memberInfo, setMemberInfo] = useState(null);
-
-  async function fetchCast() {
-    const response = await fetch('cast.json');
-    setCast(await response.json());
-  }
-
-  useEffect(() => {
-    fetchCast();
-  });
-
+  const name = 'StarGazers'
   return (
-    <>
-      <Nav cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
-      <div className="container">
+    <div className="container">
+      <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <h1>Meet the Stargazers</h1>
-          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <ListCast cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
-          {memberInfo && <Modals member={memberInfo} handleChange={(info) => { setMemberInfo(cast[info]) }} handleClose={() => { setMemberInfo(null) }} />}
+          <h1>Meet the <i style={{ color: "SteelBlue" }}>{name}</i></h1>
+          <Welcome name="Stars" />
+          <Hello name="StarGazers" />
+          <p>Members of an <b>intergalactic alliance</b><br />
+            paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <button className="outline" onClick={() => alert('Hi there')}>Click Me</button>
         </hgroup>
-      </div>
-    </>
+      </article>
+    </div>
   )
 }
 export default App
