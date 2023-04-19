5647d9c03d28e5d7513cfae526842bcb9755ab81
Sathya pramod <sathyapramod12@gmail.com>
2023-04-19 14:49:58 +0000

    Created a Class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import React, { component } from 'react';
+
+class Welcome extends React.component{
+  constructor(){
+    super();
+  }
+  render(){
+    return <h1>Hello, Welcome to react</h1>
+  }
+}
+
 function App() {
   const name = 'StarGazers'
   return (
@@ -5,7 +16,9 @@ function App() {
       <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <h1>Meet the <i style={{ color: "SteelBlue" }}>{name}</i></h1>
+          <Welcome />
+          <Welcome />
+          <Welcome />
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
           <button className="outline" onClick={() => alert('Hi there')}>Click Me</button>
