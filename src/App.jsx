407b5ea845be26dc80783dac2bb78bf129a564ca
Moises771 <julianbrb23@gmail.com>
2023-05-31 14:38:35 +0200

    added NPM

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 function App() {
+  const name = "StarGazers";
+  const wena = ()=>alert("Wena po tallarin!")
   return (
+  <div className="container">
+    <article>
     <hgroup>
-      <h1>Meet the StarGazers</h1>
+      <img src="images/group.svg" alt="StarGazers" />
+      <h1>Meet the <i style={{color: "steelblue"}}>{name}</i></h1>
       <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+      <button className="outlilne" onClick={wena}>Click me!</button>
     </hgroup>
+    </article>
+    </div>
+    
   )
 }
 export default App
