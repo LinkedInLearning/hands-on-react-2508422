f60f8d47d9da65a828f58090762788dd3bb7f519
Ajit Vaze <ajitvaze@users.noreply.github.com>
2023-06-20 12:03:21 +0000

    Made basic changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 function App() {
+  const name = 'StarGazers';
+  const nameStyle = {color:"orange"};
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
-  )
+    <div className="container">
+      <article>
+        <hgroup>
+          <img src="images/group.svg" alt="Stargazers group" />
+          <h1>Meet the <i style={nameStyle}>{ name }</i></h1>
+          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species.<br />They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <button className="outline" onClick={()=> alert('Hi There')}>
+            Click Me
+          </button>
+        </hgroup>
+      </article>
+    </div>
+  );
 }
 export default App
