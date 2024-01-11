9dc84645b294791550ffedbd68eda32da27261e8
Terrell Miller <45802369+HangoutZone@users.noreply.github.com>
2024-01-11 21:35:58 +0000

    Added some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-
+import Welcome from './components/Welcome'
+import Support from './components/Support';
 //this is a component
-class Welcome extends React.Component{
-  constructor(props) {
-    super(props);
-  }
-  render(){
-    return <h1>Meet the {this.props.name}</h1>;
-  }
-}
+// class Welcome extends React.Component{
+//   constructor(props) {
+//     super(props);
+//   }
+//   render(){
+//     return <h1>Meet the {this.props.name}</h1>;
+//   }
+// }
 
 //this is a hook
-const Hello = (props) => {
-  return <h1>Meet the <i>{props.name}</i></h1>
-}
+// const Hello = (props) => {
+//   return <h1>Meet the <i>{props.name}</i></h1>
+// }
+
 function App() {
   const name = 'StarGazers'
   return (
@@ -23,11 +25,11 @@ function App() {
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
           <Welcome name="Stars" />
-          <Hello name="StarGazers" />
+          <Welcome name="StarGazers" />
           <Welcome name="StarAliens" />
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <button className="outline" onClick={() => alert('Hi there')}>Click Me</button>
+          <Support />
         </hgroup>
       </article>
     </div>
